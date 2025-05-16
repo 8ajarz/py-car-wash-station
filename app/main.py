8e1646032f9a298dfbadfbec78c2e4f3707f94b3
8ajarz <8ajarz_zaprasza@protonmail.com>
2025-05-16 23:04:44 +0700

    passes all test and flake8 check

--- app/main.py
+++ app/main.py
@@ -1,8 +1,57 @@
 class Car:
-    # write your code here
-    pass
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str
+    ) -> None:
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(
+            self,
+            distance_from_city_center: float,
+            clean_power: object,
+            average_rating: float,
+            count_of_ratings: int) -> None:
+
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def serve_cars(self, cars: list) -> float:
+        income = sum(
+            [
+                self.calculate_washing_price(car)
+                for car in cars if car.clean_mark < self.clean_power
+            ]
+        )
+        for car in cars:
+            self.wash_single_car(car)
+        return income
+
+    def calculate_washing_price(self, car: object) -> float:
+        price = round(
+            (
+                car.comfort_class
+                * (self.clean_power - car.clean_mark)
+                * self.average_rating
+                / self.distance_from_city_center
+            ), 1)
+        return price
+
+    def wash_single_car(self, car: object) -> None:
+        if car.clean_mark < self.clean_power:
+            car.clean_mark = self.clean_power
+
+    def rate_service(self, rate: int) -> None:
+        self.count_of_ratings += 1
+        avg = (
+            self.average_rating
+            * (self.count_of_ratings - 1) + rate
+        ) / self.count_of_ratings
+        self.average_rating = round(avg, 1)
